72a851de949b2e48faca87db5738e49b8a9920a0
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-30 18:49:09 +0100

    added files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,9 +3,12 @@ console.log('This is your service-worker.js file!');
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "index.js",
+    "/index.js",
+    "/styles.css",
     "/db.js",
-    "/style.css"
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.json"
 ];
 
 const CACHE_NAME = "static-cache-v2";
