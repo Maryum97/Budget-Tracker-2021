7e10ef00ed47803c67381de85de605c0d107242a
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-30 19:44:38 +0100

    random commits

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,6 +21,7 @@ self.addEventListener('install', (event) => {
         caches.open(CACHE_NAME)
             .then((cache) => {
                 console.log('Opened cache successfully!');
+                console.log(cache);
                 return cache.addAll(FILES_TO_CACHE);
             })
     );
